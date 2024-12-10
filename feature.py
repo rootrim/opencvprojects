b2a7264ac2ff21581fd8369147f7d5e1d71204cd
gitrim <omuaz01@gmail.com>
2024-12-11 00:09:44 +0300

    Little developed
    I tried my best
    Sorry

--- feature.py
+++ feature.py
@@ -1,12 +1,32 @@
 import cv2 as cv
 import openFuncs as of
 
-of.kitty()
+
+def main(dio: bool) -> None:
+    chs = cv.imread("Photos/cukulatalar.jpg", 0)
+    cho = cv.imread("Photos/cukulata.jpg", 0)
+
+    if dio:
+        display_imgs(chs, cho, cmap="gray")
+
+    orb: cv.ORB = cv.ORB_create()
+
+    kp1, ds1 = orb.detectAndCompute(cho, None)
+    kp2, ds2 = orb.detectAndCompute(chs, None)
+
+    bf = cv.BFMatcher(cv.NORM_HAMMING)
+
+    matches = bf.match(ds1, ds2)
+    smatchs = sorted(matches, key=lambda x: x.distance)
+
+    imgMatch = cv.drawMatches(cho, kp1, chs, kp2, matches[:30], None, flags=2)
+    of.imshow(imgMatch, "gray")
 
 
-def main() -> None:
-    pass
+def display_imgs(*imgs, cmap="gray"):
+    for img in imgs:
+        of.imshow(img, cmap=cmap)
 
 
 if __name__ == "__main__":
-    main()
+    main(False)
