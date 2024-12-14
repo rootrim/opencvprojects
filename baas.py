1b9cf5e4561c20af685a6feb77bf6bdc7d188fd0
gitrim <omuaz01@gmail.com>
2024-12-14 14:46:29 +0300

    Feature Pairing and Baas alogirthm
    Finishded 2 topics yay!

---
+++ baas.py
@@ -0,0 +1,46 @@
+import cv2 as cv
+import openFuncs as of
+import numpy as np
+
+
+def main():
+    img: np.ndarray = cv.imread("Photos/dots.png")
+
+    blur: np.ndarray = cv.medianBlur(img, 13)
+
+    gray: np.ndarray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
+
+    _, threshed = cv.threshold(gray, 65, 255, cv.THRESH_BINARY)
+
+    dist = cv.distanceTransform(threshed, cv.DIST_L2, 5)
+
+    _, foreg = cv.threshold(dist, 0, 255, 0)
+
+    linux = np.ones((3, 3), np.uint8)
+
+    backg = cv.dilate(foreg, linux, iterations=1)
+    foreg = np.uint8(foreg)
+
+    known = cv.subtract(backg, foreg, dtype=0)
+
+    _, marker = cv.connectedComponents(foreg)
+    marker += 1
+    marker[known == 255] = 0
+
+    marker = cv.watershed(img, marker)
+
+    contours, hierarchy = cv.findContours(
+        marker.copy(),
+        cv.RETR_CCOMP,
+        cv.CHAIN_APPROX_SIMPLE,
+    )
+
+    for i in range(len(contours)):
+        if hierarchy[0][i][3] == -1:
+            cv.drawContours(img, contours, i, (0, 255, 0), 2)
+
+    of.imshow(img)
+
+
+if __name__ == "__main__":
+    main()
--- feature.py
+++ feature.py
@@ -2,25 +2,61 @@ import cv2 as cv
 import openFuncs as of
 
 
-def main(dio: bool) -> None:
-    chs = cv.imread("Photos/cukulatalar.jpg", 0)
-    cho = cv.imread("Photos/cukulata.jpg", 0)
+def main(dio: bool, kitty: bool) -> None:
+    if kitty:
+        of.kitty()
+
+    thepicture = cv.imread("Photos/cukulatalar.jpg", 0)
+    chocolata = cv.imread("Photos/cukulata.jpg", 0)
 
     if dio:
-        display_imgs(chs, cho, cmap="gray")
+        display_imgs(thepicture, chocolata, cmap="gray")
 
     orb: cv.ORB = cv.ORB_create()
 
-    kp1, ds1 = orb.detectAndCompute(cho, None)
-    kp2, ds2 = orb.detectAndCompute(chs, None)
+    kp1, ds1 = orb.detectAndCompute(chocolata, None)
+    kp2, ds2 = orb.detectAndCompute(thepicture, None)
 
     bf = cv.BFMatcher(cv.NORM_HAMMING)
 
     matches = bf.match(ds1, ds2)
     smatchs = sorted(matches, key=lambda x: x.distance)
 
-    imgMatch = cv.drawMatches(cho, kp1, chs, kp2, matches[:30], None, flags=2)
+    imgMatch = cv.drawMatches(
+        chocolata,
+        kp1,
+        thepicture,
+        kp2,
+        smatchs[:20],
+        None,
+        flags=2,
+    )
+
     of.imshow(imgMatch, "gray")
+
+    sift: cv.SIFT = cv.SIFT_create()
+
+    bf = cv.BFMatcher()
+
+    kp1, ds1 = sift.detectAndCompute(chocolata, None)
+    kp2, ds2 = sift.detectAndCompute(thepicture, None)
+
+    matches = bf.knnMatch(ds1, ds2, k=2)
+
+    nices = [[m for m, n in matches if m.distance < 0.75 * n.distance]]
+
+    siftMatches = cv.drawMatchesKnn(
+        chocolata,
+        kp1,
+        thepicture,
+        kp2,
+        nices,
+        None,
+        flags=2,
+    )
+
+    of.imshow(siftMatches)
+    cv.destroyAllWindows()
 
 
 def display_imgs(*imgs, cmap="gray"):
@@ -29,4 +65,4 @@ def display_imgs(*imgs, cmap="gray"):
 
 
 if __name__ == "__main__":
-    main(False)
+    main(False, False)
